2392325fe0dcb9d34b83fd91de216b3a6d06d6bf
Adrian Pearman <adrianpearman12@gmail.com>
2018-11-23 18:11:35 -0500

    renaming of client folder, completion of implementing graphql to the front end and styling from bootswatch.

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema')
 const app = express()
+const cors = require('cors')
 
-const PORT = process.env.PORT || 4000 
+const PORT = process.env.PORT || 5000 
+
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
     schema,
