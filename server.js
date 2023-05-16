916e5ef43d0c76410f82653830a532c747a44491
Adrian Pearman <adrianpearman12@gmail.com>
2023-05-15 20:04:42 -0400

    updated backend content to bring content up to date

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-const express = require('express')
-const graphqlHTTP = require('express-graphql')
-const schema = require('./schema')
-const app = express()
-const cors = require('cors')
-const path = require('path')
-
-const PORT = process.env.PORT || 5000 
-
-app.use(cors())
-
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true
-}))
-
-app.use(express.static('public'))
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-})
+// NPM Modules
+const cors = require("cors");
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const path = require("path");
+// Local Variables
+const app = express();
+const PORT = process.env.PORT || 4000;
+const schema = require("./schema");
+// Middlewares
+app.use(cors());
+app.use(express.static("public"));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
+// Serving the client files
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
-    console.log(`Running on PORT: ${PORT}`)
-})
+  console.log(`Running on PORT: ${PORT}`);
+});
